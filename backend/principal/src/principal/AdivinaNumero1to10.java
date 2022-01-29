ecde1da67259fa1ac1c8a17d167dc349c4bc1932
agomezru <agomezru@LES011533.corp.capgemini.com>
2022-01-29 20:24:20 +0100

    ejercicio1 v2

--- backend/principal/src/principal/AdivinaNumero1to10.java
+++ backend/principal/src/principal/AdivinaNumero1to10.java
@@ -6,44 +6,51 @@ import java.util.Scanner;
 public class AdivinaNumero1to10 {
 	static int numusuario;
 	static int numrandom;
-	
-	public static void adivinaelnumero() {
-		int intentos = 0;
+	static int intentos = 0;
+
+	public void inicializa() {
 		
 		Scanner  sc = new Scanner(System.in);
+		System.out.println("Ingresa un numero");
+		numusuario = sc.nextInt();
+
 		System.out.println("El numero es: "+numrandom);
+		System.out.println("El numero de intentos es: " + this.jugada());
+
 		for(int i=0; i<10; i++)	{	
-			intentos = i;
+			
 			if (numusuario==numrandom) {
-				System.out.println("Has acertado el numero " + intentos );				
+				System.out.println("Has acertado el numero " + this.jugada() );				
 				break;				
 			}
 			if (numusuario>numrandom) {
 				
-				System.out.println("El numero es menor, intentalo de nuevo" + intentos);
+				System.out.println("El numero es menor, intentalo de nuevo" + this.jugada());
 				numusuario = sc.nextInt();
 				
 			}
 			if (numusuario<numrandom) {
-				System.out.println("El numero es mayor, intentalo de nuevo"+ intentos);
+				System.out.println("El numero es mayor, intentalo de nuevo"+ this.jugada());
 				numusuario = sc.nextInt();
 			}	
 			
 		}
-		System.out.println("No has conseguido hacer los 10 intentos"+ intentos);				
+		if (intentos==10)
+			System.out.println("No has conseguido hacer los 10 intentos"+ this.jugada());				
 		sc.close();
+	}
+	
+	public String jugada() {
+		return "El numero de intentos son "+ ++intentos;
 	}
 
 	
 	public static void main(String[] args) {
 		System.out.println("Bienvenido al juego de advinar numeros pon un numero del 1 al 100");
-		Scanner sc = new Scanner(System.in);
 		Random rd = new Random();
-		String cadena = sc.nextLine();
-		numusuario = Integer.parseInt(cadena);
 		numrandom = rd.nextInt(100);	
-		adivinaelnumero();
-		sc.close();
+		AdivinaNumero1to10 ad = new AdivinaNumero1to10(); 
+		ad.inicializa();
 		
 	}
 	
