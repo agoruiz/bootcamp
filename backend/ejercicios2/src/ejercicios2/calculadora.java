b371c90cdb75a0b1f868080de92782ef8de3c1e0
agomezru <agomezru@host.docker.internal>
2022-01-31 13:41:31 +0100

    para ver los fallos

--- backend/ejercicios2/src/ejercicios2/calculadora.java
+++ backend/ejercicios2/src/ejercicios2/calculadora.java
@@ -8,9 +8,17 @@ public class calculadora {
 public static void main(String args[]) {
 	String cadena = "3+4+3,4-7*1";
 	String separados[] =cadena.split("(?<=[-+*=])");
+	int numero1;
+	int numero2;
+	int operador;
+	char caracter;
 	
 for (int i=0; i<separados.length; i++) {
-	System.out.println("La cadena es: " + separados[i]);
+	for (int j=0; j<separados[i].length(); j++) {
+	 	caracter = separados[j].charAt(j);
+	 	System.out.println("El caracter es "+ caracter);
+		
+	}
 }
 	
 	
