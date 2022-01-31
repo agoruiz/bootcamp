96726876074afc2a0f6578f8b9615a49218e9cd9
agomezru <agomezru@host.docker.internal>
2022-01-31 11:25:27 +0100

    mejora en la cadena con split

--- backend/ejercicios2/src/ejercicios2/calculadora.java
+++ backend/ejercicios2/src/ejercicios2/calculadora.java
@@ -1,6 +1,5 @@
 package ejercicios2;
 
-import java.nio.charset.Charset;
 
 public class calculadora {
 	/*  3+4+3,4-7*1= decodificar cadena
@@ -8,18 +7,13 @@ public class calculadora {
 */
 public static void main(String args[]) {
 	String cadena = "3+4+3,4-7*1";
-	char caracter;
-	int posicion=0;
-	for (int i=0; i<cadena.length(); i++) 
-	{ 
-		 caracter = cadena.charAt(posicion);
-		 if (caracter == '+'  || caracter == '-' || caracter =='*' ) { 
-			 cadena.lines();
-		 }
-		 if(caracter ==',') {
-			 posicion++;
-		 }
-	}
+	String separados[] =cadena.split("(?<=[-+*=])");
+	
+for (int i=0; i<separados.length; i++) {
+	System.out.println("La cadena es: " + separados[i]);
+}
+	
+	
 	
 }
 
