55ae17bca71d7e5e74dafec7fe8035f48ddbb915
agomezru <agomezru@LES011533.corp.capgemini.com>
2022-02-07 22:05:41 +0100

    Creacion de concepto de baraja

--- backend/Naipes/src/Baraja.java
+++ backend/Naipes/src/Baraja.java
@@ -1,4 +1,30 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 
-interface Baraja{
-	enum numeroCartas {};
-}
+interface JuegosdeCartas {
+	  
+  }
+
+  interface Baraja<T> extends JuegosdeCartas{
+	  int NumeroCartas=0;
+	  
+	  public void rellenar();
+	  public void devuelveCartas();
+	  public boolean Barajar();
+	  public boolean Repartir();
+  }
+  
+  interface Naipe {
+	 int valor=0;
+	 int palo=0;
+	 HashMap<String, String> cartas = new HashMap<String, String>();
+  }
+  
+  class Española implements Baraja, Naipe{
+	  
+  }
+  
+  class Francesa implements Baraja, Naipe{
+	  
+  }
--- backend/Naipes/src/TipoBarajas.java
+++ backend/Naipes/src/TipoBarajas.java
@@ -4,9 +4,9 @@ enum tipoBarajas {Española, Inglesa};
 enum valor{};
 enum palo{};
 
- boolean asignaryValorPalo (int valor,int palo); 
+ boolean asignaryValorPalo (int valor,int palo) throws Throwable; 
  boolean añadirvalor(int valor ) throws Throwable;  
- tipoBarajas propiedadesBaraja();
+ tipoBarajas propiedadesBaraja() throws Throwable;
 boolean añadirvalor(int palo, int valor) throws Throwable;
 
 }
