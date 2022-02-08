39bb079b0f81b9a77f87c95476739cceecc5f956
agomezru <agomezru@host.docker.internal>
2022-02-08 12:20:04 +0100

    version de dia 8

--- backend/Naipes/src/Baraja.java
+++ backend/Naipes/src/Baraja.java
@@ -1,30 +1,79 @@
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
-interface JuegosdeCartas {
+/*Baraja contiene un list de naipes
+ */
+  abstract class Baraja<T>{
+	  List<T> naipe= new ArrayList<T>();
+	  public abstract boolean Barajar();
+	  public abstract boolean Repartir();
+  }
+  
+  abstract class Naipe<T> {	 
+	  abstract HashMap<T, Integer> rellenarbaraja();
+  }
+  
+	  
+  
+  
+  class Española extends Naipe{
+	int numerocartas = 48;
+	int numeropalos = 4;
+	int valormaxcarta =10;
+	
+	enum palos {Bastos, Copas, Oros,Espadas};
+	HashMap<palos,Integer> cartas = new HashMap<palos, Integer>();
+	/*La Baraja española se compone de 4 palos en los cuales contiene 10 cartas 
+	 * numeradas del 1 al 10
+	 */
+	@Override
+	public HashMap rellenarbaraja() {
+		for(int i=0; i<numeropalos; i++) {
+			for(int j=0; j<valormaxcarta; j++) {
+				if(i==0) {
+					cartas.put(palos.Bastos, j+1);
+				}
+				else if(i==1) {
+					cartas.put(palos.Copas, j+1);
+				}
+				else if(i==2) {
+					cartas.put(palos.Espadas, j+1);
+				}
+				else if(i==3) {
+					cartas.put(palos.Oros, j+1);
+				}
+			}
+			
+			
+		}
+		return cartas;
+	}
+
+
+	@Override
+	public boolean Barajar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean Repartir() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	
+	  
+  
+  
+ 
 	  
   }
 
-  interface Baraja<T> extends JuegosdeCartas{
-	  int NumeroCartas=0;
-	  
-	  public void rellenar();
-	  public void devuelveCartas();
-	  public boolean Barajar();
-	  public boolean Repartir();
-  }
+
   
-  interface Naipe {
-	 int valor=0;
-	 int palo=0;
-	 HashMap<String, String> cartas = new HashMap<String, String>();
-  }
   
-  class Española implements Baraja, Naipe{
-	  
-  }
   
-  class Francesa implements Baraja, Naipe{
-	  
-  }
