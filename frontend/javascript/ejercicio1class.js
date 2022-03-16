939b4c57163e998aa7fe3c95e56f48f0838fd1fb
agomezru <agomezru@LES011533.corp.capgemini.com>
2022-03-16 13:17:01 +0100

    para revisar

---
+++ frontend/javascript/ejercicio1class.js
@@ -0,0 +1,30 @@
+
+
+class adivina (Number){
+    constructor(number){
+        this.number = Number;
+    }
+let final = Math.random()*100*numero1;
+
+for(var i=10; i>0; i--){
+
+    alert("te quedan "+i+" intentos");
+    this.numero1= window.prompt("meter numero");
+
+if(numero1>final){
+    alert("El numero 1 es mayor");
+    i++;
+}
+ if (numero1==final){
+    alert("Has acertado");
+    break;
+}
+ if(numero1>final){
+    alert("El numero 1 es mayor");
+    i++;
+}
+}
+
+}
+function getadivina(){
+}
---
+++ frontend/javascript/ejercicio1obj.js
@@ -0,0 +1,28 @@
+
+
+
+function adivina(number){
+let final = Math.random()*100*numero1;
+
+for(var i=10; i>0; i--){
+
+    alert("te quedan "+i+" intentos");
+    this.numero1= window.prompt("meter numero");
+
+if(numero1>final){
+    alert("El numero 1 es mayor");
+    i++;
+}
+ if (numero1==final){
+    alert("Has acertado");
+    break;
+}
+ if(numero1>final){
+    alert("El numero 1 es mayor");
+    i++;
+}
+}
+
+}
+adivina ad = new adivina(10);
+console.log(ad.numero1);
