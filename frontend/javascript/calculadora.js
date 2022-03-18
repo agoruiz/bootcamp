be27b1b4f64640f290c8d4a223fd4a7cf864bdce
agomezru <agomezru@LES011533.corp.capgemini.com>
2022-03-18 19:51:45 +0100

    trabajo por hoy

--- frontend/javascript/calculadora.js
+++ frontend/javascript/calculadora.js
@@ -2,51 +2,19 @@ let valornumero;
 let campo;
 let valor;
 let num1;
+
+
 document.getElementById("button1").addEventListener("click", btn1);
 function btn1() {
+    alert("funciona");
 num1 = document.getElementById("button1").value();
 }
+
 
 document.getElementById("button2").addEventListener("click", btn2);
 function btn2() {
 num1 = document.getElementById("button2").value();
 }
-
-document.getElementsByClassName("button3").addEventListener("click", btn3);
-function btn3() {
-num1 = document.getElementsByClassName("button3").value();
-}
-
-document.getElementsByClassName("button4").addEventListener("click", btn4);
-function btn4() {
-num1 = document.getElementsByClassName("button4").value();
-}
-
-document.getElementsByClassName("button5").addEventListener("click", btn5);
-function btn5() {
-num1 = document.getElementsByClassName("button5").value();
-}
-
-document.getElementsByClassNam("button6").addEventListener("click", btn6);
-function btn6() {
-num1 = document.getElementsByClassName("button6").value();
-}
-
-document.getElementsByClassNam("button7").addEventListener("click", btn7);
-function btn6() {
-num1 = document.getElementsByClassName("button7").value();
-}
-
-document.getElementsByClassNam("button8").addEventListener("click", btn8);
-function btn8() {
-num1 = document.getElementsByClassName("button8").value();
-}
-
-document.getElementsByClassNam("button9").addEventListener("click", btn9);
-function btn9() {
-num1 = document.getElementsByClassName("button9").value();
-}
-
 
 
 let opsumar = document.getElementById("sumar")
